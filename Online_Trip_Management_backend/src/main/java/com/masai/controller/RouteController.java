9fa69ff41f51548e26a5352b459520c96ebd2da7
RounakDhatterwal <115460483+RounakDhatterwal@users.noreply.github.com>
2023-06-03 00:04:47 +0530

    done with controlller

--- Online_Trip_Management_backend/src/main/java/com/masai/controller/RouteController.java
+++ Online_Trip_Management_backend/src/main/java/com/masai/controller/RouteController.java
@@ -1,16 +1,64 @@
 package com.masai.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.masai.entity.Route;
+import com.masai.exception.LoginException;
+import com.masai.exception.RouteException;
 import com.masai.service.RouteService;
 
-@RestController
+import jakarta.validation.Valid;
+
+@RestController("/online/trip/route")
 @CrossOrigin("*")
 public class RouteController {
 	@Autowired
 	private RouteService routeService;
 	
+	@PostMapping("/add/{uuid}")
+	public ResponseEntity<Route> addRoute(@Valid @RequestBody Route route, @PathVariable String uuid) throws LoginException, RouteException{
+		Route ru = routeService.addRoute(uuid, route);
+		return new ResponseEntity<Route>(ru, HttpStatus.OK);
+	}
 	
+	@PutMapping("update/{uuid}")
+	public ResponseEntity<Route> updateRoute(@Valid @RequestBody Route route, @PathVariable String uuid) throws LoginException, RouteException{
+		Route ru = routeService.updateRoute(uuid, route);
+		return new ResponseEntity<Route>(ru, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("remove/{uuid}/{routeid}")
+	public ResponseEntity<Route> removeRoute(@PathVariable String uuid, @PathVariable int routeid) throws LoginException, RouteException{
+		Route ru = routeService.removeRoute(uuid, routeid);
+		return new ResponseEntity<Route>(ru, HttpStatus.OK);
+	}
+	
+	@GetMapping("search/{routeid}")
+	public ResponseEntity<Route> searchRoute(@PathVariable int routeid) throws LoginException, RouteException{
+		Route ru = routeService.searchRoute(routeid);
+		return new ResponseEntity<Route>(ru, HttpStatus.OK);
+	}
+	
+	@GetMapping("allroutes")
+	public ResponseEntity<List<Route>> allRoute() throws LoginException, RouteException{
+		List<Route> list = routeService.viewRouteList();
+		return new ResponseEntity<List<Route>>(list, HttpStatus.OK);
+	}
+	
+	
+	
+	
+
 }
