52ba2da3e68bd4baa2ee0771360fb2bc32bf4489
RounakDhatterwal <115460483+RounakDhatterwal@users.noreply.github.com>
2023-06-03 00:32:17 +0530

    sessionId name changed

--- Online_Trip_Management_backend/src/main/java/com/masai/entity/CurrentUserSession.java
+++ Online_Trip_Management_backend/src/main/java/com/masai/entity/CurrentUserSession.java
@@ -3,6 +3,8 @@ package com.masai.entity;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,7 +23,8 @@ public class CurrentUserSession {
 	
 	@Id
 	private Integer userId;
-	private String uuid;
+	private String sessionId;
+	@Enumerated(EnumType.STRING)
 	private Role role;
 	private LocalDateTime loginTime;
 }
