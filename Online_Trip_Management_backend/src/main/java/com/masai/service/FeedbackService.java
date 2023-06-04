3c85740e21c04f192d42ccbf26053f7bf59cbf7c
RounakDhatterwal <115460483+RounakDhatterwal@users.noreply.github.com>
2023-06-04 23:41:50 +0530

    feedback conroller

--- Online_Trip_Management_backend/src/main/java/com/masai/service/FeedbackService.java
+++ Online_Trip_Management_backend/src/main/java/com/masai/service/FeedbackService.java
@@ -7,8 +7,9 @@ import com.masai.entity.Feedback;
 import com.masai.exception.CustomerException;
 import com.masai.exception.FeedbackException;
 import com.masai.exception.LoginException;
+import com.masai.exception.PackageException;
 
 public interface FeedbackService {
 	public FeedbackResponse addFeedback(String sessionId, Feedback feedback) throws LoginException, CustomerException, FeedbackException;
-	public List<Feedback> getFeedbackByPackageId(int packageid) throws FeedbackException;
+	public List<Feedback> getFeedbackByPackageId(String sessionId, int packageid) throws LoginException, PackageException,  FeedbackException;
 }	
--- Online_Trip_Management_backend/src/main/java/com/masai/service/FeedbackServiceImpl.java
+++ Online_Trip_Management_backend/src/main/java/com/masai/service/FeedbackServiceImpl.java
@@ -11,9 +11,11 @@ import com.masai.entity.CurrentUserSession;
 import com.masai.entity.Customer;
 import com.masai.entity.Feedback;
 import com.masai.entity.Package;
+import com.masai.entity.CurrentUserSession.Role;
 import com.masai.exception.CustomerException;
 import com.masai.exception.FeedbackException;
 import com.masai.exception.LoginException;
+import com.masai.exception.PackageException;
 import com.masai.repository.CustomerRepository;
 import com.masai.repository.FeedbackRepository;
 import com.masai.repository.PackageRepository;
@@ -29,6 +31,8 @@ public class FeedbackServiceImpl implements FeedbackService{
 	private CustomerRepository customerRepo;
 	@Autowired
 	private PackageRepository packageRepo;
+	@Autowired
+	private EmailsenderService emailService;
 	
 	@Override
 	public FeedbackResponse addFeedback(String sessionId, Feedback feedback) throws FeedbackException, LoginException, CustomerException {
@@ -44,13 +48,31 @@ public class FeedbackServiceImpl implements FeedbackService{
 		Optional<Package> pack = packageRepo.findById(feedback.getPackageId()) ;
 		pack.get().getFeedbacks().add(feedback);
 		feedbackRepo.save(feedback);
+		
+		// Send an email .
+		String emailSubject = "Feedback Response";
+		String interviewerBody = "Dear " +us.getUsername()+"," +"\n\n thank you for your valueable feedback.";
+		String email = us.getEmail();
+		emailService.sendEmail(email, emailSubject, interviewerBody);
+		
 		return new FeedbackResponse(LocalDateTime.now(), "Feedback sucessfully submitted");
 	}
 
 	@Override
-	public List<Feedback> getFeedbackByPackageId(int packageid) throws FeedbackException {
+	public List<Feedback> getFeedbackByPackageId(String sessionId, int packageid) throws FeedbackException, PackageException, LoginException{
 		// TODO Auto-generated method stub
-		return null;
+		CurrentUserSession cus = sessRepo.findBySessionId(sessionId);
+		if (cus == null)
+			throw new LoginException("You're not logged in");
+		if (cus.getRole() != Role.ADMIN) {
+			throw new LoginException("Incorrect Credentials");
+		}
+		
+		Optional<Package> pack = packageRepo.findById(packageid);
+		if(pack==null) throw new PackageException("Package not found with the same id");
+		List<Feedback> list = pack.get().getFeedbacks();
+		if(list==null) throw new FeedbackException("Feedback not found");
+		return list;
 	}
 	
 }
